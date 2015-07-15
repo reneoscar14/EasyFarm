557da9864844dbc59539505ea72af1a9a7a0b767
EDOS <reneoscar14@gmail.com>
2015-07-14 21:26:30 -0400

    Fixing Error 0.2

--- Controllers/InvernaderoController.cs
+++ Controllers/InvernaderoController.cs
@@ -19,7 +19,7 @@ namespace EasyFarm.Controllers
         {
             return View(db.Invernaderoes.ToList());
         }
-        
+        public void test() { }
         public ActionResult Thomas()
         {
             return View(db.Invernaderoes.First());
