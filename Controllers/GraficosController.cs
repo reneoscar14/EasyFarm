03942a4677a9296a213a6c18045bacea7288f953
EDOS <reneoscar14@gmail.com>
2015-11-22 22:33:05 -0400

    22-11-2015
    se agrego 2 link de camara

---
+++ Controllers/GraficosController.cs
@@ -0,0 +1,24 @@
+﻿using EasyFarm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyFarm.Controllers
+{
+    public class GraficosController : Controller
+    {
+        private EasyFarmContext db = new EasyFarmContext();
+        //
+        // GET: /Graficos/
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+
+	}
+}
